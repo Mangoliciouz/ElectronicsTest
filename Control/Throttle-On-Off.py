bd3a1e3e0a3bad1b031632f066a4bc08f14f1f00
Mangoliciouz <31861001+Mangoliciouz@users.noreply.github.com>
2017-09-11 21:29:48 +0100

    First File - Pi Motor Test
    Quick to test run motor forwards and backward at full speed.

---
+++ Control/Throttle-On-Off.py
@@ -0,0 +1,48 @@
+#Set Motor Direction to Pin 7 & Power to Pin 8. LOW = Forward
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BOARD)
+
+MotorDir = 7
+MotorPwr = 8
+
+GPIO.setup(MotorDir,GPIO.OUT)
+GPIO.setup(MotorPwr,GPIO.OUT)
+
+print "Turning Motor on Forwards for 5 Seconds."
+
+sleep(2)
+
+GPIO.output(MotorDir,GPIO.LOW)
+GPIO.output(MotorPwr,GPIO.HIGH)
+
+sleep(5)
+
+GPIO.output(MotorPwr,GPIO.LOW)
+
+sleep(2)
+
+print "Turning Motor on backwards for 5 Seconds."
+
+sleep(2)
+
+GPIO.output(MotorDir,GPIO.HIGH)
+GPIO.output(MotorPwr,GPIO.LOW)
+
+sleep(5)
+
+GPIO.output(MotorPwr,GPIO.LOW)
+
+sleep(2)
+
+GPIO.output(MotorDir,GPIO.LOW)
+
+print "Test Complete."
+
+GPIO.cleanup()
+
+sleep(2)
+
+exit()
