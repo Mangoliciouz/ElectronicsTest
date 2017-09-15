6201e9088c4c7da8c505ac54f9435533b7c11808
Mangoliciouz <31861001+Mangoliciouz@users.noreply.github.com>
2017-09-15 23:46:08 +0100

    TCP Throttle - Change recv

--- Control/TCP-Throttle-Pi-Variable.py
+++ Control/TCP-Throttle-Pi-Variable.py
@@ -28,7 +28,7 @@ print('[*] Started listening ', ip,':',port)
 (client, addr) = server.accept()
 print('[*] Got a connection from ', addr[0],':',addr[1])
 while True:
-    data = client.recv(1024)
+    data = client.recv()
     print('[*] Recieved ',data,' From the client')
     print('    Proccessing data')
 
